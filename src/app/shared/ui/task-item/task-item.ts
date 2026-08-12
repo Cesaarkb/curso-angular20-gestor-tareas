51e8a3dcf3b4e81d6fa771c0c5d7dc8c065884a0
cesaarkb <cesaarkb@gmail.com>
2026-08-11 18:59:58 -0600

    Clase 27 julio

---
+++ src/app/shared/ui/task-item/task-item.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+import { Task } from '../../../features/tasks/task';
+
+@Component({
+  selector: 'app-task-item',
+  imports: [],
+  templateUrl: './task-item.html',
+  styleUrl: './task-item.css',
+})
+export class TaskItem {
+
+  task = input.required<Task>();
+
+  toggle = output<number>();
+  removed = output<number>();
+
+}
